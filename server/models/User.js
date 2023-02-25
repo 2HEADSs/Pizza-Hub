27c5f8c1ffaa084b23b738c0f77db881b4eb2edc
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-02-25 16:03:17 +0200

    add user model

---
+++ server/models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+//todo email validator
+const userSchema = new Schema({
+    email: { type: String, required: true, unique: true },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [5, "Username must be at least 5 characters!"],
+        maxlength: [10, "Username cannot have more than 10 characters!"],
+    },
+    hashedPassword: { type: String, required: true },
+});
+
+userSchema.index(
+    { email: 1 },
+    {
+        collation: {
+            locale: "en",
+            strength: 2,
+        },
+    }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
