aca80d85c97506569c06f4b88cc4bc8e1665e3e2
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-02-25 15:07:50 +0200

    start server on port 3000

---
+++ server/connectionString.js
@@ -0,0 +1,4 @@
+const connectionString = "mongodb://127.0.0.1:27017/";
+module.exports = {
+    connectionString,
+};
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+// const cors =
+const { connectionString } = require("./connectionString");
+const { mongoose } = require("mongoose");
+const router = require("./routes");
+
+const initDB = () => mongoose.connect(connectionString);
+
+startServer();
+async function startServer() {
+    initDB();
+    app.use(express.json());
+
+    app.use(router);
+    app.listen("3000", () => console.log("REST listening on port 3000"));
+}
