6e1d5a8b471f003ab663a1ae8e13e2b397ff8d6f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-02-27 12:47:26 +0200

    update controller with post method

--- server/controllers/pizzaController.js
+++ server/controllers/pizzaController.js
@@ -1,10 +1,11 @@
 const pizzaController = require("express").Router();
 
-const { getAll } = require("../services/pizzaService");
+const { getAll, create } = require("../services/pizzaService");
 
 pizzaController.get("/", async (req, res) => {
     try {
         const pizza = await getAll();
+        console.log('get');
         res.status(200).json(pizza);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -12,11 +13,15 @@ pizzaController.get("/", async (req, res) => {
 });
 
 pizzaController.post("/", async (req, res) => {
+    console.log(req.body);
     try {
-        const pizza = await getAll();
-        res.status(200).json(pizza);
+        const pizza = await create(req.body);
+        console.log(pizza);
+        res.json(pizza);
+        // res.status(200).json(pizza);
     } catch (error) {
         res.status(400).json({ error: error.message });
+        console.log(error);
     }
 });
 
