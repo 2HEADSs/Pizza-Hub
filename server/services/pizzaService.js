11c138ea8ca2c7ea456c02a5963e2e8f303df121
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-04 13:53:40 +0200

    add more service functionality

--- server/services/pizzaService.js
+++ server/services/pizzaService.js
@@ -4,12 +4,67 @@ async function getAll() {
     return Pizza.find({});
 }
 
+async function getByUserId(userId) {
+    return Pizza.find({ _ownerId: userId })
+
+};
+async function getById(id) {
+    return Pizza.findById(id).populate('_ownerId')
+};
+
 async function create(data) {
     //TODO add user(creator) to pizza
     return Pizza.create(data)
 };
 
+async function getMyLikes(id) {
+    const pizzas = await Pizza.find({})
+    let arr = [];
+    pizzas.map(x => {
+        if (!!(x.likes.includes(id))) {
+            arr.push(x)
+        }
+    })
+    return arr;
+
+}
+
+async function update(id, pizza) {
+    const existing = await Pizza.findById(id);
+
+    existing.name = pizza.name;
+    existing.type = pizza.type;
+    existing.ingrediants = pizza.ingrediants;
+    existing.prepTime = pizza.prepTime;
+    existing.cookTime = pizza.cookTime;
+    existing.recipe = pizza.recipe;
+    existing.description = pizza.description;
+    existing.img = pizza.img;
+    return existing.save()
+}
+
+
+async function deleteById(id) {
+    return Pizza.findByIdAndDelete(id)
+};
+
+// async function getMyPizza(id) {
+//     return await Pizza.find({ _ownerId: id })
+// }
+
+async function likePizza(pizzaId, userId) {
+    const existing = await Pizza.findById(pizzaId)
+    existing.likes.push(userId);
+    return existing.save()
+}
 module.exports = {
     getAll,
-    create
+    getByUserId,
+    getById,
+    create,
+    update,
+    getMyLikes,
+    deleteById,
+    // getMyPizza,
+    likePizza
 };
