f1ece63d7c6ee4181c38dfa419a349bec24b47ca
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-04 14:06:07 +0200

    add more controllers

--- server/controllers/pizzaController.js
+++ server/controllers/pizzaController.js
@@ -1,6 +1,6 @@
 const pizzaController = require("express").Router();
 
-const { getAll, create } = require("../services/pizzaService");
+const { getAll, create, getById, getByUserId, getMyLikes, likePizza } = require("../services/pizzaService");
 
 pizzaController.get("/", async (req, res) => {
     try {
@@ -8,6 +8,37 @@ pizzaController.get("/", async (req, res) => {
         res.status(200).json(pizza);
     } catch (error) {
         res.status(400).json({ error: error.message });
+    }
+});
+
+pizzaController.get('/my-pizzas', async (req, res) => {
+    const pizza = await getByUserId(req.user._id);
+    res.status(200).json(pizza)
+});
+
+pizzaController.get('/my-likes', async (req, res) => {
+
+    try {
+        const pizza = await getMyLikes(req.user._id)
+        return res.status(200).json(pizza)
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message })
+    }
+})
+
+
+pizzaController.get('/:id', async (req, res) => {
+    try {
+        const pizza = await getById(req.params.id)
+        if (!pizza) {
+            throw new Error('Pizza does not exist')
+            
+        }
+        return res.status(200).json(pizza)
+    } catch (error) {
+        res.status(400).json({ error })
+        
     }
 });
 
@@ -23,20 +54,57 @@ pizzaController.post("/", async (req, res) => {
 });
 
 
+pizzaController.put('/:id', async (req, res) => {
+    try {
+        const pizza = await getById(req.params.id);
 
-pizzaController.get('/:id', async (req, res) => {
+        // todo parse token
+        if (req.user._id != pizza._ownerId._id) {
+            return res.status(403).json({ message: 'You cannot modify this record' })
+        }
+        const result = await update(req.params.id, req.body);
+        res.status(200).json(result)
+    } catch (err) {
+        console.log(err);
+        // const message = parseError(err)
+        res.status(400).json({ error: err.message })
+    }
+});
+
+
+pizzaController.delete('/:id', async (req, res) => {
+    try {
+        const pizza = await getById(req.params.id);
+        if (req.user._id != pizza._ownerId._id) {
+            return res.status(403).json({ err: err.message })
+        }
+        await deleteById(req.params.id);
+        res.status(204).end()
+    } catch (err) {
+        res.status(400).json({ err: err.message })
+    }
+});
+
+pizzaController.get('/like/:id', async (req, res) => {
     try {
         const pizza = await getById(req.params.id)
-        if (!pizza) {
-            throw new Error('Pizza does not exist')
+        if (pizza._ownerId._id != req.user._id
+            && pizza.likes.map(x => x.includes(req.user?._id) == false)) {
+            try {
+                await likePizza(req.params.id, req.user._id);
+                const pizza = await getById(req.params.id)
 
+                return res.status(200).json(pizza)
+            } catch (error) {
+                res.status(400).json({ err: error.message })
+            }
         }
-        return res.status(200).json(bike)
     } catch (error) {
-        res.status(400).json({ error })
+        res.status(400).json({ err: error.message })
 
     }
 });
+
 
 module.exports = {
     pizzaController
