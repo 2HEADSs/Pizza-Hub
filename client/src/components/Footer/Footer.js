db4001e0a4e4a28337a5ff0508b0aae68a1ec36a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-05 13:26:56 +0200

    update footer ingo

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import './Footer.css'
 export const Footer = () => {
     return (
         <footer className='footer'>
-            <p>Copyright © 2018 Luxury Home ™ All Rights Reserved</p>
+            <p>P@vel Dimitrov personal REACT SPA</p>
             <div className="icons">
                 <a><i class="fa-solid fa-envelope"></i></a>
                 <a><i class="fa-brands fa-square-github"></i></a>
