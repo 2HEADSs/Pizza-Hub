de92430556d1dfab55e31835d7ca7da128f0869b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-05 12:05:28 +0200

    add footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+export const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p>Copyright © 2018 Luxury Home ™ All Rights Reserved</p>
+            <div className="icons">
+                <a><i class="fa-solid fa-envelope"></i></a>
+                <a><i class="fa-brands fa-square-github"></i></a>
+                <a><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+        </footer>
+    )
+}
