2e04a07335fd2afd9f4f7f96101c398688986676
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-05 14:35:24 +0200

    update style

--- client/src/components/singlePizza/SinglePizza.js
+++ client/src/components/singlePizza/SinglePizza.js
@@ -3,8 +3,10 @@ export const SinglePiza = ({ pizza }) => {
     return (
         <article className="single-pizza">
             <img src={pizza.img} alt={pizza.name} />
-            <p> {pizza.type}</p>
-            <p> {pizza.name}</p>
+            <div className='info-pizza-container'>
+                <p>Type: <span>{pizza.type}</span> </p>
+                <p>Name: <span>{pizza.name}</span> </p>
+            </div>
         </article>
 
     )
