97cf23e1bcb4d841b9f97c0c71a1fca8ea83665e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-11 11:53:40 +0200

    Create Catalog.js

---
+++ client/src/components/Catalog/Catalog.js
@@ -0,0 +1,23 @@
+import './Catalog.css'
+import { useEffect, useState } from "react";
+import * as pizzaService from '../../pizza-services'
+import { SinglePiza } from "../singlePizza/SinglePizza";
+
+export const Catalog = () => {
+    const [allPizas, setPizza] = useState([]);
+    useEffect(() => {
+        pizzaService.getAll()
+            .then(pizzas => setPizza(pizzas))
+    }, [])
+
+    return (
+        <section className="catalog">
+            <h2>All Pizzas</h2>
+            <article className='card-wrapper'>
+                {allPizas.map(pizza =>
+                    <SinglePiza pizza={pizza} key={pizza._id} />
+                )}
+            </article>
+        </section>
+    )
+                }
