fac02b1117d3e07fa56e285eb1f531ccf6e1a29a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-18 13:25:01 +0200

    remo unnecceasary model from server

--- server/models/Pizza.js
+++ server/models/Pizza.js
@@ -42,12 +42,6 @@ const pizzaSchema = new Schema({
         minlength: [10, "Recipe must be minimum ten characters!"],
         maxlength: [500, "Recipe must be maximum hundred characters!"],
     },
-    description: {
-        type: String,
-        required: true,
-        minlength: [10, "Description must be minimum ten characters!"],
-        maxlength: [500, "Description must be maximum hundred characters!"],
-    },
     img: {
         type: String,
         validate: {
