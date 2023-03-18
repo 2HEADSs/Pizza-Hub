759e014a1d4e5b45882c2512befb5edd7feabdfa
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-18 17:15:25 +0200

    fix path

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -1,6 +1,6 @@
 import './Catalog.css'
 import { useEffect, useState } from "react";
-import * as pizzaService from '../../pizza-services'
+import * as pizzaService from '../../services/pizza-services'
 import { SinglePiza } from "../singlePizza/SinglePizza";
 
 export const Catalog = () => {
