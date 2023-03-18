0e5cd7a464b1fed47c4137e2bb603b6aabba8790
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-18 10:53:51 +0200

    create simple login form with demo css

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,22 @@
 import './Login.css'
 export const Login = () => {
     return (
-  <h1>LOGIN</h1>
+      <div id="login-form-wrap">
+      <h2>Login</h2>
+      <form id="login-form">
+          <p>
+              <input type="text" id="username" name="username" placeholder="Username" required/><i class="validation"><span></span><span></span></i>
+          </p>
+          <p>
+              <input type="email" id="email" name="email" placeholder="Email Address" required/><i class="validation"><span></span><span></span></i>
+          </p>
+          <p>
+              <input type="submit" id="login" value="Login"/>
+          </p>
+      </form>
+      <div className="already-have-account-wrapper">
+          <p>Not a member? <a href="#">Create Account</a></p>
+          </div>
+      </div>
     )
 }
