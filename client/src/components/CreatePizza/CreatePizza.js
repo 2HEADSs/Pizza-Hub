3ae71d99ab74938852fd0c4cc096bfdbeb1113c7
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-18 16:48:12 +0200

    add controlled forms

--- client/src/components/CreatePizza/CreatePizza.js
+++ client/src/components/CreatePizza/CreatePizza.js
@@ -1,21 +1,84 @@
+import { useState } from 'react'
+
 import './CreatePizza.css'
 export const CreatePizza = () => {
+    const [pizzaData, setPizzaData] = useState({
+        name: '',
+        type: '',
+        ingrediants: '',
+        prepTime: '',
+        cookTime: '',
+        img: '',
+        recipe: ''
+    });
+    const createPizzaHandler = (e) => {
+        e.preventDefault()
+        console.log(pizzaData);
+    }
+
+    const addPizzaData = (e) => {
+        setPizzaData({ ...pizzaData, [e.target.name]: e.target.value })
+
+    }
     return (
         <div className="create-form-wrap">
             <h2>Create your precious </h2>
-            <form className="create-form">
-                <input type="text" className="name" name="name" placeholder="Name of your pizza" />
-                <input type="text" className="type" name="type" placeholder="MUST BE DROPDOWN" />
-                <input 
-                type="text" 
-                className="ingrediants" 
-                name="ingrediants"
-                    placeholder="Write your ingrediants separete with comma: ',' "
+            <form className="create-form" onSubmit={createPizzaHandler}>
+                <input
+                    type="text"
+                    className="name"
+                    name="name"
+                    placeholder="Name of your pizza"
+                    value={pizzaData.name}
+                    onChange={addPizzaData}
                 />
-                <input type="number" className="prepTime" name="prepTime" placeholder="Preptime" />
-                <input type="number" className="cookTime" name="cookTime" placeholder="Cooktime" />
-                <input type="text" className="img" name="img" placeholder="Add link with image " />
-                <textarea className="recipe" name="recipe" placeholder="Describe preparation..."></textarea>
+                <input
+                    type="text"
+                    className="type"
+                    name="type"
+                    placeholder="MUST BE DROPDOWN"
+                    value={pizzaData.type}
+                    onChange={addPizzaData}
+                />
+                <input
+                    type="text"
+                    className="ingrediants"
+                    name="ingrediants"
+                    placeholder="Write your ingrediants separete with comma: ','"
+                    value={pizzaData.ingrediants}
+                    onChange={addPizzaData}
+                />
+                <input
+                    type="number"
+                    className="prepTime"
+                    name="prepTime"
+                    placeholder="Preptime"
+                    value={pizzaData.prepTime}
+                    onChange={addPizzaData}
+                />
+                <input
+                    type="number"
+                    className="cookTime"
+                    name="cookTime"
+                    placeholder="Cooktime"
+                    value={pizzaData.cookTime}
+                    onChange={addPizzaData}
+                />
+                <input
+                    type="text"
+                    className="img"
+                    name="img"
+                    placeholder="Add link with image"
+                    value={pizzaData.img}
+                    onChange={addPizzaData}
+                />
+                <textarea
+                    className="recipe"
+                    name="recipe"
+                    placeholder="Describe preparation..."
+                    value={pizzaData.recipe}
+                    onChange={addPizzaData}
+                ></textarea>
 
                 <input type="submit" className="login" value="Push in main branch" />
             </form>
