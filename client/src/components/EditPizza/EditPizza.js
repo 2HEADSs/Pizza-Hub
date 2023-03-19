d4bda8176914d7fff3614e8858f8faa141c9332f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-19 17:06:50 +0200

    Update EditPizza.js

--- client/src/components/EditPizza/EditPizza.js
+++ client/src/components/EditPizza/EditPizza.js
@@ -1,17 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import * as pizzaService from '../../services/pizza-services'
+import { useParams } from 'react-router-dom';
 
 import './EditPizza.css'
+
 export const EditPizza = () => {
-    const [pizzaData, setPizzaData] = useState({
-        name: '',
-        type: '',
-        ingrediants: '',
-        prepTime: '',
-        cookTime: '',
-        img: '',
-        recipe: ''
-    });
+    const { pizzaId } = useParams();
+    console.log(pizzaId);
+
+    useEffect(() => {
+        pizzaService.getOnePizza(pizzaId)
+            .then(result => {
+                setPizzaData(result)
+            })
+    }, [pizzaId])
+
+
+    const [pizzaData, setPizzaData] = useState({});
+
+
+
     const createPizzaHandler = async (e) => {
         e.preventDefault()
         console.log(pizzaData);
@@ -23,9 +31,10 @@ export const EditPizza = () => {
     };
 
     return (
-        <div className="create-form-wrap">
-            <h2>Create your precious </h2>
-            <form className="create-form" onSubmit={createPizzaHandler}>
+        <div className="edit-form-wrap">
+            <h2>Edit your precious... </h2>
+            <form className="edit-form" onSubmit={createPizzaHandler}>
+                <label for="name">Name:</label>
                 <input
                     type="text"
                     className="name"
@@ -34,14 +43,16 @@ export const EditPizza = () => {
                     value={pizzaData.name}
                     onChange={addPizzaData}
                 />
+                <label for="type">Type:</label>
                 <input
                     type="text"
                     className="type"
                     name="type"
                     placeholder="MUST BE DROPDOWN"
-                    value={pizzaData.type}
+                    value={`Pizza type:  ${pizzaData.type}`}
                     onChange={addPizzaData}
                 />
+                <label for="ingrediants">Ingrediants:</label>
                 <input
                     type="text"
                     className="ingrediants"
@@ -50,6 +61,7 @@ export const EditPizza = () => {
                     value={pizzaData.ingrediants}
                     onChange={addPizzaData}
                 />
+                <label for="prepTime">Preparation time:</label>
                 <input
                     type="number"
                     className="prepTime"
@@ -58,6 +70,7 @@ export const EditPizza = () => {
                     value={pizzaData.prepTime}
                     onChange={addPizzaData}
                 />
+                <label for="cookTime">Cooking time:</label>
                 <input
                     type="number"
                     className="cookTime"
@@ -66,6 +79,7 @@ export const EditPizza = () => {
                     value={pizzaData.cookTime}
                     onChange={addPizzaData}
                 />
+                <label for="img">Image Link</label>
                 <input
                     type="text"
                     className="img"
@@ -74,6 +88,8 @@ export const EditPizza = () => {
                     value={pizzaData.img}
                     onChange={addPizzaData}
                 />
+                <label for="recipe">Describe preparation...</label>
+
                 <textarea
                     className="recipe"
                     name="recipe"
@@ -82,7 +98,7 @@ export const EditPizza = () => {
                     onChange={addPizzaData}
                 ></textarea>
 
-                <input type="submit" className="login" value="Push in main branch" />
+                <input type="submit" className="login" value="Edit Pizza" />
             </form>
         </div>
     )
