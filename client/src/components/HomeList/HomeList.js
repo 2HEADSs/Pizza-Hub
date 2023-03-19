393a7000ea1870cc3041627825df1feee6f0b541
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-19 12:04:32 +0200

    create HomeList component

---
+++ client/src/components/HomeList/HomeList.js
@@ -0,0 +1,23 @@
+import './HomeList.css'
+import { useEffect, useState } from "react";
+import * as pizzaService from '../../services/pizza-services'
+import { SinglePiza } from "../singlePizza/SinglePizza";
+
+export const HomeList = () => {
+    const [allPizas, setPizza] = useState([]);
+    useEffect(() => {
+        pizzaService.getAll()
+            .then(pizzas => setPizza(pizzas))
+    }, [])
+
+    return (
+        <section className="last-added">
+            <h2>Last added</h2>
+            <article className='card-wrapper'>
+                {allPizas.slice(-5 ).map(pizza =>
+                    <SinglePiza pizza={pizza} key={pizza._id} />
+                )}
+            </article>
+        </section>
+    )
+}
