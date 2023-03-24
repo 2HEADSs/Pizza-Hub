54f85502006e95f53bca45b9650c357a3b2b4443
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-24 21:43:24 +0200

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,13 +1,14 @@
 const baseUrl = 'http://localhost:3030/auth';
 
 
-export const login = async () => {
+export const login = async (data) => {
     try {
         const response = await fetch(`${baseUrl}/login`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
-            }
+            },
+            body: JSON.stringify(data)
         })
     } catch (error) {
         console.log(error);
