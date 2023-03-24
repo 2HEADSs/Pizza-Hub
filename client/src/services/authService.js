9fbd81731d0c95a88e45aa0a57353b54b8e07280
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-24 22:03:38 +0200

    add register service

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -18,3 +18,22 @@ export const authLogin = async (data) => {
     }
 
 }
+
+
+export const authRegister = async (data) => {
+    try {
+        const response = await fetch(`${baseUrl}/register`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.log(error);
+        return error
+    }
+
+}
