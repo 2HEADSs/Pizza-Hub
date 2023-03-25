4b3396dc2df1f5b3a31852e51160dbdf0506eb66
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-25 22:02:47 +0200

    Update pizzaService.js

--- client/src/services/pizzaService.js
+++ client/src/services/pizzaService.js
@@ -8,7 +8,6 @@ export const getAll = async () => {
         const result = await response.json();
         return result
     } catch (error) {
-        console.log(`${error} = errrorrrr`);
         return [];
     }
 
@@ -21,14 +20,14 @@ export const getOnePizza = async (pizzaId) => {
     return result
 }
 
-export const create = async (pizza) => {
-    console.log(pizza);
+export const create = async (pizza, _ownerId) => {
+    const data = { ...pizza, _ownerId }
     const response = await fetch(`${baseUrl}/pizza`, {
         method: 'POST',
         headers: {
             'content-type': 'application/json',
         },
-        body: JSON.stringify(pizza)
+        body: JSON.stringify(data)
     });
     const result = await response.json();
     return result
