9c7bf09325677a096d32bf25d3521ba94cad01bd
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-26 14:49:05 +0300

    Update Header.js

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,13 +8,13 @@ import './Header.css';
 export const Header = () => {
     const { user } = useContext(AuthContext);
     const isAuth = user?._id ? true : false;
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const { setUserSession } = useContext(AuthContext);
 
     const logoutFn = () => {
         setUserSession({});
         localStorage.clear();
-        navigate('/');
+        // navigate('/');
     }
     return (
         <div className='header-wrapper'>
@@ -46,7 +46,7 @@ export const Header = () => {
                                     <Link className='link' to="/create">Add Pizza</Link>
                                 </li>
                                 <li className='link-nav'>
-                                    <Link className='link' onClick={logoutFn}>Logout</Link>
+                                    <Link className='link' to="/" onClick={logoutFn}>Logout</Link>
                                 </li>
                             </>
                         )}
