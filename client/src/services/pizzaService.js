c1515ce5ab56fd7a1f5ea6fbafad8c973ba73f62
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-26 11:23:58 +0300

    Update pizzaService.js

--- client/src/services/pizzaService.js
+++ client/src/services/pizzaService.js
@@ -21,14 +21,26 @@ export const getOnePizza = async (pizzaId) => {
 }
 
 export const create = async (pizza, _ownerId) => {
-    const data = { ...pizza, _ownerId }
-    const response = await fetch(`${baseUrl}/pizza`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(data)
-    });
-    const result = await response.json();
-    return result
+    const data = { ...pizza, _ownerId };
+
+    try {
+        const response = await fetch(`${baseUrl}/pizza`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        if (response.ok) {
+            return result
+        } else {
+
+            throw new Error(result.error);
+        }
+    } catch (error) {
+        return error
+    }
+
+
 }
