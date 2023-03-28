7730fe72328c8b8ce5232da1e26d99996340a3af
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-28 10:17:51 +0300

    remove todo/s

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-//todo email validator
 // const EMAIL_PATTERN = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 const emailRegex = /^[a-zA-Z0-9._%+-]{3,}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const userSchema = new Schema({
