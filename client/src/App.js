665f478e5c8f4205b4cee63264dc9dbcafc24379
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-03-31 21:34:01 +0300

    add guards for guest and lgged in users

--- client/src/App.js
+++ client/src/App.js
@@ -14,35 +14,44 @@ import { EditPizza } from './components/EditPizza/EditPizza';
 import { useLocalStorage } from './hooks/localStorage';
 import { MyPizzas } from './components/MyPizzas/MyPizzas';
 import { MyFavourite } from './components/MyFavourite/MyFavourite';
+import { GuestGuard } from './guards/GuestGuard';
+import { UserGuard } from './guards/UserGuard';
 
 
 function App() {
-  const [user, setUser] = useLocalStorage('auth', {})
+    const [user, setUser] = useLocalStorage('auth', {})
 
-  const setUserSession = (data) => {
-    setUser({ ...data })
-  }
+    const setUserSession = (data) => {
+        setUser({ ...data })
+    }
 
-  return (
-    <>
-      <AuthContext.Provider value={{ setUserSession, user }}>
-        <Header />
-        <Routes>
-          <Route path='/' element={<HomeList />} />
-          <Route path='/catalog' element={<Catalog />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/my-pizza' element={<MyPizzas />} />
-          <Route path='/my-favourite' element={<MyFavourite />} />
-          <Route path='/create' element={<CreatePizza />} />
-          <Route path='/catalog/details/:pizzaId' element={<PizzaDetails />} />
-          <Route path='/catalog/edit/:pizzaId' element={<EditPizza />} />
-        </Routes>
-      </AuthContext.Provider>
-      <Footer />
-    </>
+    return (
+        <>
+            <AuthContext.Provider value={{ setUserSession, user }}>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<HomeList />} />
+                    <Route path='/catalog' element={<Catalog />} />
+                    <Route path='/catalog/details/:pizzaId' element={<PizzaDetails />} />
 
-  );
+                    <Route element={<UserGuard />}>
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                    </Route>
+
+                    <Route element={<GuestGuard />}>
+                        <Route path='/my-pizza' element={<MyPizzas />} />
+                        <Route path='/my-favourite' element={<MyFavourite />} />
+                        <Route path='/create' element={<CreatePizza />} />
+                        <Route path='/catalog/edit/:pizzaId' element={<EditPizza />} />
+                    </Route>
+
+                </Routes>
+            </AuthContext.Provider>
+            <Footer />
+        </>
+
+    );
 }
 
 export default App;
