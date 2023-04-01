d6df8336e7840dc13ef3f1e0b778e8464d861a72
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-01 18:44:17 +0300

    add loading spinner to myFavourite component

--- client/src/components/MyFavourite/MyFavourite.js
+++ client/src/components/MyFavourite/MyFavourite.js
@@ -5,26 +5,47 @@ import * as pizzaService from '../../services/pizzaService'
 import { SinglePiza } from '../SinglePizza/SinglePizza';
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import { Loading } from "../Loading/Loading";
 
 
 export const MyFavourite = () => {
     const [myLikedPizzas, setmyLikedPizzas] = useState([]);
+    const [loaded, setLoaded] = useState(true);
+    const [hasItem, setHasItems] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
     const { user } = useContext(AuthContext);
 
     useEffect(() => {
         pizzaService.getMyLikes(user.accessToken)
-            .then(pizzas => setmyLikedPizzas(pizzas))
+            .then(data => {
+                if (data.length > 0) {
+                    setLoaded(false)
+                    setHasItems(true)
+                    setIsEmpty(false)
+                } else {
+                    setLoaded(false)
+                    setIsEmpty(true)
+                }
+                setmyLikedPizzas(data)
+            });
     }, []);
 
     return (
         <section className="my-favourite">
-            <h2>Your favourites</h2>
-            <article className='card-wrapper'>
-                {myLikedPizzas.map(pizza =>
-                    <SinglePiza pizza={pizza} key={pizza._id} />
-                )}
-            </article>
-            {myLikedPizzas.length === 0 && (
+            {hasItem && (
+                <>
+                    <h2>Your favourites</h2>
+                    <article className='card-wrapper'>
+                        {myLikedPizzas.map(pizza =>
+                            <SinglePiza pizza={pizza} key={pizza._id} />
+                        )}
+                    </article>
+                </>
+            )}
+            {loaded && (
+                <Loading />
+            )}
+            {isEmpty && (
                 <h3>Maybe you don't like pizza!</h3>
             )}
         </section>
