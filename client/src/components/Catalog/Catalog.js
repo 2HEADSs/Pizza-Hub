83521a16f3ea3db85e992ab816034b1daaec0b97
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-01 16:55:32 +0300

    update loading functionality

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -7,22 +7,37 @@ import { Loading } from '../Loading/Loading';
 
 export const Catalog = () => {
     const [allPizas, setPizza] = useState([]);
+    const [loaded, setLoaded] = useState(true);
+    const [hasItems, setHasItems] = useState(true)
     useEffect(() => {
         pizzaService.getAll()
-            .then(pizzas => setPizza(pizzas))
+            .then(pizzas => {
+                // setPizza(pizzas.splice(0, 5))
+                setPizza([])
+                if (pizzas.length > 0) {
+                    setLoaded(false)
+                } else {
+                    setLoaded(false)
+                    setHasItems(false)
+                }
+            });
     }, []);
     return (
         <section className="catalog">
             <h2>All Pizzas</h2>
             <article className='card-wrapper'>
-                {allPizas.map(pizza =>
-                    <SinglePiza pizza={pizza} key={pizza._id} />
+                {hasItems && (
+                    allPizas.map(pizza =>
+                        <SinglePiza pizza={pizza} key={pizza._id} />
+                    )
                 )}
             </article>
-            {allPizas.length === 0 && (
+            {loaded && (
                 <Loading />
-            )
-            }
+            )}
+            {!hasItems && (
+                <h1>no content</h1>
+            )}
         </section >
     );
 }
