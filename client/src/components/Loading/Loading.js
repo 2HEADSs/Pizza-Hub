5bfb06ce3fd4fc9f8bddbb05ddbb08cbef028504
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-01 18:09:23 +0300

    change css of loading component

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -2,7 +2,7 @@ import './Loading.css';
 export const Loading = () => {
     return (
         <>
-            <h2>Loading....Please wait....</h2>
+            <h2 className='loading-header'>Loading....Please wait....</h2>
             <div className="lds-ellipsis">
                 <div></div>
                 <div></div>
