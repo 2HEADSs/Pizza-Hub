013af13a4189275b831de64be74b27af409e5182
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-02 18:27:15 +0300

    style header of MyPizzas component

--- client/src/components/MyPizzas/MyPizzas.js
+++ client/src/components/MyPizzas/MyPizzas.js
@@ -2,29 +2,48 @@ import { useEffect, useState } from "react";
 
 import './MyPizzas.css'
 import * as pizzaService from '../../services/pizzaService'
-import { SinglePiza } from '../SinglePizza/SinglePizza';
+import {  SinglePizza } from '../SinglePizza/SinglePizza';
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import { Loading } from "../Loading/Loading";
 
 
 export const MyPizzas = () => {
     const [myPizzas, setMyPizzas] = useState({});
+    const [loaded, setLoaded] = useState(true);
+    const [hasItem, setHasItems] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
     const { user } = useContext(AuthContext);
 
     useEffect(() => {
         pizzaService.getMyPizza(user.accessToken)
-            .then(pizzas => setMyPizzas(pizzas))
+            .then(data => {
+                if (data.length > 0) {
+                    setLoaded(false)
+                    setHasItems(true)
+                    setIsEmpty(false)
+                } else {
+                    setLoaded(false)
+                    setIsEmpty(true)
+                }
+                setMyPizzas(data)
+            })
     }, []);
 
     return (
         <section className="catalog">
-            <h2>{user.username} pizzas</h2>
-            <article className='card-wrapper'>
-                {myPizzas.map(pizza =>
-                    <SinglePiza {...pizza} key={pizza._id} />
-                )}
-            </article>
-            {myPizzas.length === 0 && (
+            <h2 className="my-pizzas-header">Your precious</h2>
+            {hasItem && (
+                <article className='card-wrapper'>
+                    {myPizzas.map(pizza =>
+                        <SinglePizza {...pizza} key={pizza._id} />
+                    )}
+                </article>
+            )}
+            {loaded && (
+                <Loading />
+            )}
+            {isEmpty && (
                 <h3>You don't have own pizzas!!!</h3>
             )}
         </section>
