c588c924d761ea00290091bac09d00f9b656a9e0
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-06 18:49:12 +0300

    using atlas for database

--- server/connectionString.js
+++ server/connectionString.js
@@ -1,4 +1,5 @@
-const connectionString = "mongodb://127.0.0.1:27017/pizzahub";
+// const connectionString = "mongodb://127.0.0.1:27017/pizzahub";
+const connectionString = "mongodb+srv://2HEADS:1234567890@cluster0.69tuqnn.mongodb.net/pizza-hub";
 module.exports = {
     connectionString,
 };
