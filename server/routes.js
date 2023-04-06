f2547466cbba76f12f93816ba569af6893b7e8bd
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-06 22:38:40 +0300

    add logs for deploying

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
 const { pizzaController } = require("./controllers/pizzaController");
-const {authController} = require("./controllers/authController")
+const { authController } = require("./controllers/authController")
 
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+    console.log(req);
     res.json({ message: "Rest Service Operational" });
 });
 
