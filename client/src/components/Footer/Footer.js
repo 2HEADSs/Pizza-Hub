9304c2d7935ab02d90070486e36926165e2124c9
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-06 22:11:21 +0300

    add links to linkedin and github in footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import './Footer.css'
 export const Footer = () => {
     return (
         <footer className='footer'>
             <p>P@vel Dimitrov personal REACT SPA</p>
             <div className="icons">
-                <a><i className="fa-solid fa-envelope"></i></a>
-                <a><i className="fa-brands fa-square-github"></i></a>
-                <a><i className="fa-brands fa-linkedin"></i></a>
+                <Link ><i className="fa-solid fa-envelope"> </i></ Link>
+                <Link to="https://github.com/2HEADSs"><i className="fa-brands fa-square-github"></i></Link>
+                <Link to="https://www.linkedin.com/in/pavel-dimitrov-b6a503229/"><i className="fa-brands fa-linkedin"></i></Link>
             </div>
         </footer>
     )
