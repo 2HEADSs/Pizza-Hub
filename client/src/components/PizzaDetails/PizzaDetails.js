5138415f279266112c55dbe975fac8d2fd4ce590
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-07 12:32:42 +0300

    add loader in details component

--- client/src/components/PizzaDetails/PizzaDetails.js
+++ client/src/components/PizzaDetails/PizzaDetails.js
@@ -6,12 +6,15 @@ import * as pizzaService from '../../services/pizzaService'
 
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import { Loading } from '../Loading/Loading';
+
 
 export const PizzaDetails = () => {
     const [alreadyLiked, setAlreadyLiked] = useState(undefined);
-    const [pizza, setPizza] = useState({});
+    const [pizza, setPizza] = useState([]);
     const [showConfirm, setShowConfirm] = useState(false);
     const [isShown, setIsShown] = useState(false);
+    const [loaded, setLoaded] = useState(true);
 
 
     const navigate = useNavigate();
@@ -21,11 +24,16 @@ export const PizzaDetails = () => {
     useEffect(() => {
         pizzaService.getOnePizza(pizzaId)
             .then(result => {
+                if (result.length > 0) {
+                    setLoaded(false)
+                    setAlreadyLiked(result?.likes?.includes(user?._id));
+                } else {
+                    setLoaded(false)
+                }
                 setPizza(result)
-                setAlreadyLiked(result?.likes?.includes(user?._id));
             })
             .catch(err => console.log(err))
-    }, [pizzaId]);
+    }, []);
 
 
     const isOwner = user?._id === pizza?._ownerId?._id;
@@ -59,63 +67,69 @@ export const PizzaDetails = () => {
             console.log(error);
         }
     }
-
     return (
+        <>
+            {loaded && (
+                <Loading />
+            )}
+            {!loaded > 0 && (
+                < section className="details-wrapper" >
+                    <img src={pizza.img} alt={pizza.name} className="details-img" />
+                    <article className="details-info">
 
-        < section className="details-wrapper" >
-            <img src={pizza.img} alt={pizza.name} className="details-img" />
-            <article className="details-info">
+                        <h2>Name: <span>{pizza.name}</span></h2>
+                        <h3>Made by: <span>{pizza?._ownerId?.username}</span></h3>
+                        <p>Type: <span>{pizza.type}</span></p>
+                        <p>Ingrediants: <span>{pizza.ingrediants}</span></p>
+                        <div className="times">
+                            <i className="fa-regular fa-clock"></i>
+                            <p>Prep: <span>{pizza.prepTime} minutes</span></p>
+                            <i className="fa-regular fa-clock"></i>
+                            <p>Cook: <span>{pizza.cookTime} minutes</span></p>
+                        </div>
+                        <p>How to prepare: <span>{pizza.recipe}</span></p>
 
-                <h2>Name: <span>{pizza.name}</span></h2>
-                <h3>Made by: <span>{pizza?._ownerId?.username}</span></h3>
-                <p>Type: <span>{pizza.type}</span></p>
-                <p>Ingrediants: <span>{pizza.ingrediants}</span></p>
-                <div className="times">
-                    <i className="fa-regular fa-clock"></i>
-                    <p>Prep: <span>{pizza.prepTime} minutes</span></p>
-                    <i className="fa-regular fa-clock"></i>
-                    <p>Cook: <span>{pizza.cookTime} minutes</span></p>
-                </div>
-                <p>How to prepare: <span>{pizza.recipe}</span></p>
-
-                {isOwner && (
-                    <>
-                        <Link to={`/catalog/edit/${pizza._id}`} className="edit-link">Edit</Link>
-                    </>
-                )}
-                {alreadyLiked && (
-                    <h3 className="allready-liked">Already liked! </h3>
-
-                )}
-                {(!alreadyLiked && hasUser && alreadyLiked !== undefined && !isOwner) && (
-                    <>
-
-                        <a className="add-to-favourite-heart" onClick={likeHandler} onMouseEnter={() => setIsShown(true)}
-                            onMouseLeave={() => setIsShown(false)}><i className="fa-solid fa-heart"></i></a>
-
-                        {isShown && (
-                            <h3 className="add-to-favourite-info">Click on heart add in your favourite list! </h3>
+                        {isOwner && (
+                            <>
+                                <Link to={`/catalog/edit/${pizza._id}`} className="edit-link">Edit</Link>
+                            </>
                         )}
-                    </>
-                )}
-                {(alreadyLiked && hasUser && alreadyLiked !== undefined && !isOwner) && (
-                    <>
-                        <button onClick={deleteFromFavourite} className="deleteFromFavourite">Delete from favourite</button>
+                        {alreadyLiked && (
+                            <h3 className="allready-liked">Already liked! </h3>
 
-                    </>
-                )}
-                {isOwner && !showConfirm && (
-                    <button onClick={confirmHandler} className="delete-btn">Delete</button>
+                        )}
+                        {(!alreadyLiked && hasUser && alreadyLiked !== undefined && !isOwner) && (
+                            <>
 
-                )}
-                {showConfirm && (
-                    <>
-                        <p className="confirm-question">Are you sure?</p>
-                        <button onClick={deleteHandler} className="delete-confirm">YES</button>
-                        <button onClick={confirmHandler} className="delete-not-consfirm">NO</button>
-                    </>
-                )}
-            </article>
-        </section >
+                                <a className="add-to-favourite-heart" onClick={likeHandler} onMouseEnter={() => setIsShown(true)}
+                                    onMouseLeave={() => setIsShown(false)}><i className="fa-solid fa-heart"></i></a>
+
+                                {isShown && (
+                                    <h3 className="add-to-favourite-info">Click on heart add in your favourite list! </h3>
+                                )}
+                            </>
+                        )}
+                        {(alreadyLiked && hasUser && alreadyLiked !== undefined && !isOwner) && (
+                            <>
+                                <button onClick={deleteFromFavourite} className="deleteFromFavourite">Delete from favourite</button>
+
+                            </>
+                        )}
+                        {isOwner && !showConfirm && (
+                            <button onClick={confirmHandler} className="delete-btn">Delete</button>
+
+                        )}
+                        {showConfirm && (
+                            <>
+                                <p className="confirm-question">Are you sure?</p>
+                                <button onClick={deleteHandler} className="delete-confirm">YES</button>
+                                <button onClick={confirmHandler} className="delete-not-consfirm">NO</button>
+                            </>
+                        )}
+                    </article>
+                </section >
+            )}
+
+        </>
     )
 }
