18a405c0178c470835c74b05a6d32a039f4cd057
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-07 12:32:29 +0300

    add error handler in search component

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -27,98 +27,99 @@ export const Search = () => {
                 }
                 setsearchedPizza(data);
                 setSearchResult(data)
-            });
-    }, []);
+            })
+            .catch(err => console.log(err));
+}, []);
 
 
-    const searchHandler = (e) => {
-        e.preventDefault();
-        sethasNoSearchedResult(true)
-        if (searchData.type === "" && searchData.name === "") {
-            setSearchResult(searchedPizza);
-        }
-
-        if (searchData.type !== "" && searchData.name === "") {
-            setSearchResult(searchedPizza
-                .filter(pizza => pizza.type === searchData.type));
-        }
-
-        if (searchData.type !== "" && searchData.name !== "") {
-            setSearchResult(searchedPizza
-                .filter(pizza => pizza.type === searchData.type &&
-                    pizza.name.toLowerCase()
-                        .includes(searchData.name.toLowerCase())));
-        }
-
-        if (searchData.type === "" && searchData.name !== "") {
-            setSearchResult(searchedPizza
-                .filter(pizza => pizza.name.toLowerCase().includes(searchData.name.toLowerCase())));
-        }
-
+const searchHandler = (e) => {
+    e.preventDefault();
+    sethasNoSearchedResult(true)
+    if (searchData.type === "" && searchData.name === "") {
+        setSearchResult(searchedPizza);
     }
 
-    const addSearchData = (e) => {
-        setsearchData({ ...searchData, [e.target.name]: e.target.value });
-    };
+    if (searchData.type !== "" && searchData.name === "") {
+        setSearchResult(searchedPizza
+            .filter(pizza => pizza.type === searchData.type));
+    }
+
+    if (searchData.type !== "" && searchData.name !== "") {
+        setSearchResult(searchedPizza
+            .filter(pizza => pizza.type === searchData.type &&
+                pizza.name.toLowerCase()
+                    .includes(searchData.name.toLowerCase())));
+    }
+
+    if (searchData.type === "" && searchData.name !== "") {
+        setSearchResult(searchedPizza
+            .filter(pizza => pizza.name.toLowerCase().includes(searchData.name.toLowerCase())));
+    }
+
+}
+
+const addSearchData = (e) => {
+    setsearchData({ ...searchData, [e.target.name]: e.target.value });
+};
 
 
 
-    return (
-        <>
-            <section className='serch-section'>
-                <h2 className='serch-section_header'>Find your next meal...</h2>
-                <article className='search-article'>
-                    <form className='search-form' onSubmit={searchHandler}>
+return (
+    <>
+        <section className='serch-section'>
+            <h2 className='serch-section_header'>Find your next meal...</h2>
+            <article className='search-article'>
+                <form className='search-form' onSubmit={searchHandler}>
 
-                        <div className='search-name-wrapper'>
-                            <label htmlFor="name">Search by name:</label>
-                            <input type="text"
-                                name='name'
-                                value={searchData.name}
-                                onChange={addSearchData} />
-                        </div>
+                    <div className='search-name-wrapper'>
+                        <label htmlFor="name">Search by name:</label>
+                        <input type="text"
+                            name='name'
+                            value={searchData.name}
+                            onChange={addSearchData} />
+                    </div>
 
-                        <div className='search-type-wrapper'>
-                            <label htmlFor="type">Search by type:</label>
-                            <select className="search-type"
-                                name="type" value={searchData.type}
-                                onChange={addSearchData}
-                            >
-                                <option onChange={addSearchData} value="">No prefer</option>
-                                <option onChange={addSearchData} value="Tasty">Tasty</option>
-                                <option onChange={addSearchData} value="Whole grain">Whole grain</option>
-                                <option onChange={addSearchData} value="Vegan">Vegan</option>
-                                <option onChange={addSearchData} value="Gluten free">Gluten free</option>
-                            </select>
-                        </div>
+                    <div className='search-type-wrapper'>
+                        <label htmlFor="type">Search by type:</label>
+                        <select className="search-type"
+                            name="type" value={searchData.type}
+                            onChange={addSearchData}
+                        >
+                            <option onChange={addSearchData} value="">No prefer</option>
+                            <option onChange={addSearchData} value="Tasty">Tasty</option>
+                            <option onChange={addSearchData} value="Whole grain">Whole grain</option>
+                            <option onChange={addSearchData} value="Vegan">Vegan</option>
+                            <option onChange={addSearchData} value="Gluten free">Gluten free</option>
+                        </select>
+                    </div>
 
-                        <input type="submit" className="search-submit" value="SEARCH" />
-                    </form>
-                </article>
-            </section>
-            <section className='search-result'>
+                    <input type="submit" className="search-submit" value="SEARCH" />
+                </form>
+            </article>
+        </section>
+        <section className='search-result'>
 
-                <article className='card-wrapper'>
-                    {searchResult.map(pizza =>
-                        <SinglePizza key={pizza._id} {...pizza} />
-                    )}
-                </article>
-                {searchResult.length === 0 && hasNoSearchedResult && (
-                    <h2 className='search-result_no-result'>No result</h2>
+            <article className='card-wrapper'>
+                {searchResult.map(pizza =>
+                    <SinglePizza key={pizza._id} {...pizza} />
                 )}
-                {
-                    loaded && (
-                        <section className='search-result_search-loader'>
-                            <Loading />
-                        </section>
-                    )
-                }
-                {
-                    isEmpty && (
-                        <h2 className='no-server-content-header'>There is nothing to show!!!</h2>
-                    )
-                }
-            </section>
-        </>
-    )
+            </article>
+            {searchResult.length === 0 && hasNoSearchedResult && (
+                <h2 className='search-result_no-result'>No result</h2>
+            )}
+            {
+                loaded && (
+                    <section className='search-result_search-loader'>
+                        <Loading />
+                    </section>
+                )
+            }
+            {
+                isEmpty && (
+                    <h2 className='no-server-content-header'>There is nothing to show!!!</h2>
+                )
+            }
+        </section>
+    </>
+)
 }
