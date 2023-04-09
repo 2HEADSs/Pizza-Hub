285532c9cf676cafcd76babf7a2e5f2406bac2ba
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-09 11:55:56 +0300

    fic icon className

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer className='footer'>
             <p>P@vel Dimitrov personal REACT SPA</p>
             <div className="icons">
-                <Link to="https://www.facebook.com/profile.php?id=100010592597191"><i class="fa-brands fa-facebook-f"></i></ Link>
+                <Link to="https://www.facebook.com/profile.php?id=100010592597191"><i className="fa-brands fa-facebook-f"></i></ Link>
                 <Link to="https://github.com/2HEADSs"><i className="fa-brands fa-square-github"></i></Link>
                 <Link to="https://www.linkedin.com/in/pavel-dimitrov-b6a503229/"><i className="fa-brands fa-linkedin"></i></Link>
             </div>
