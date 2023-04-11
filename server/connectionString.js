9ae9bd31e317954c05245d69f5430204c317695d
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-11 15:15:22 +0300

    update connection strong to work in development and production environment

--- server/connectionString.js
+++ server/connectionString.js
@@ -1,6 +1,15 @@
-// const connectionString = "mongodb://127.0.0.1:27017/pizzahub";
-const connectionString = "mongodb+srv://2HEADS:1234567890@cluster0.69tuqnn.mongodb.net/pizza-hub";
-//TODO ENV FOR DEPLOY
+
+let connectionString = "";
+
+if (process.env.NODE_ENV === "production") {
+  // use Atlas DB
+  connectionString = "mongodb+srv://2HEADS:1234567890@cluster0.69tuqnn.mongodb.net/pizza-hub";
+} else {
+  // use local DB
+  connectionString = "mongodb://127.0.0.1:27017/pizzahub";
+
+}
+
 module.exports = {
-    connectionString,
+  connectionString,
 };
