f0037b536c1ed1e2767ee7ce65f3a7f2577d25e2
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-11 10:37:56 +0300

    update server

--- server/connectionString.js
+++ server/connectionString.js
@@ -1,14 +1,15 @@
 // const connectionString = "mongodb://127.0.0.1:27017/pizzahub";
 // const connectionString = "mongodb+srv://2HEADS:1234567890@cluster0.69tuqnn.mongodb.net/pizza-hub";
-const connectionString = () => {
+// const connectionString = "mongodb+srv://2HEADS:1234567890@cluster0.69tuqnn.mongodb.net/pizza-hub";
+//TODO ENV FOR DEPLOY
+
+export const connectionString = () => {
     console.log(process.env);
     return "mongodb+srv://2HEADS:1234567890@cluster0.69tuqnn.mongodb.net/pizza-hub"
 }
-// const connectionString = "mongodb+srv://2HEADS:1234567890@cluster0.69tuqnn.mongodb.net/pizza-hub";
-//TODO ENV FOR DEPLOY
-module.exports = {
-    connectionString,
-};
+// module.exports = {
+//     connectionString,
+// };
 
 
 // const mongoose = require('mongoose');
